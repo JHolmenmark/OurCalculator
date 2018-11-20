1744f147cfd4c3f187a2eb830fe79f9dd2161819
Ramisa <ramisa.tahsin98gmail.com>
2018-11-20 10:15:31 +0100

    Code added by Ramisa

--- OurCalculator.java
+++ OurCalculator.java
@@ -1,3 +1,16 @@
-public class OurCalculator {
+import java.util.Scanner;
+import java.util.InputMismatchException;
+import java.lang.NumberFormatException;
+import java.text.DecimalFormat;
 
-}
+public class OurCalculator {
+  private static double  firstNumber     =    0;
+  private static double  secondNumber    =    0;
+  private static double  answer          =    0;
+  private static String  operator        = "-1";
+  private static boolean continueLoop    = true;
+  private static Scanner scan            = new Scanner(System.in);
+  private static DecimalFormat precision = new DecimalFormat("#0.00");
+
+  public static final String ANSI_CYAN  = "\u001B[36m";
+  public static final String ANSI_RESET = "\u001B[0m";
